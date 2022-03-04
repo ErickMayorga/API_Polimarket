61adbdabba8732a7716091fd5564c1d21af86932
Erick Mayorga <erick.mayorga@epn.edu.ec>
2022-03-03 23:27:21 -0500

    update/ insert categorias y productos

---
+++ insertCategorias.sql
@@ -0,0 +1,17 @@
+/*
+INSERT INTO Categoria (nombre) VALUES 
+('Lacteos'),
+('Cocina'),
+('Embutidos'),
+('Aseo Personal'),
+('Limpieza del Hogar'),
+('Libros'),
+('Snacks'),
+('Licores'),
+('Utiles Escolares'),
+('Decoraciones'),
+('Revistas'),
+('Juguetes'),
+('Condimentos')
+*/
+SELECT * FROM Categoria
---
+++ insertProductos.sql
@@ -0,0 +1,21 @@
+/*
+--LACTEOS(1)
+INSERT INTO Producto (nombre, codigo, precio, idCategoria) VALUES
+('Leche Deslactosada 1L', '3HHQ7zZ', 1.75, 1),
+('Dulce de leche 500g', '35MWzIy', 3.25, 1),
+('Yogurt Botellita Fresa', '3hB2L99', 0.75, 1)
+
+--EMBUTIDOS(3)
+INSERT INTO Producto (nombre, codigo, precio, idCategoria) VALUES
+('Salchichas Picatidas', '3hCiu81', 4.75, 3),
+('Mortadela Tipo II Plumrose', '3pzskvC', 2.50, 3)
+
+-- CARNES(14)
+INSERT INTO Producto (nombre, codigo, precio, idCategoria) VALUES
+('Carne Cordon Blue', '3vzwTKo', 8.50, 14),
+('Nuggets de palmito', '3IJd9aK', 1.50, 14),
+('Nuggets de pollo y jamón', '3MmkwqX', 2.00, 14),
+('Hamburguesa Asada', '3MmkwqX', 2.00, 14)
+*/
+
+SELECT * FROM Producto
